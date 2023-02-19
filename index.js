729f5a238c0d7c7b2a7812ace89a4ce38954387b
danielthane <dthane8294@hotmail.co.uk>
2023-02-19 16:52:55 +0000

    Tip button functionality is working

---
+++ index.js
@@ -0,0 +1,45 @@
+"use strict";
+//Selecting Variables
+let billCostEl = document.querySelector("#bill");
+let peopleEl = document.querySelector("#number-people");
+let billStartingVal = billCostEl.value;
+let people = peopleEl.value;
+let tipPercentage = 0;
+let tip;
+let finalBill;
+
+// Dynamically Change the Bill Value without tip
+billCostEl.addEventListener("input", () => {
+  billStartingVal = parseFloat(billCostEl.value);
+  console.log(billStartingVal);
+});
+
+//Dynamically Change the people value
+peopleEl.addEventListener("input", () => {
+  people = parseFloat(peopleEl.value);
+  if (people > 0) {
+    tip = billStartingVal * (tipPercentage / 100);
+    updateResults();
+  }
+});
+
+//Add event listeners to each tip button
+document.querySelectorAll(".tip").forEach((item) => {
+  item.addEventListener("click", () => {
+    tipPercentage = item.innerHTML.split("%")[0];
+    console.log(tipPercentage);
+  });
+});
+
+function updateResults() {
+  document.getElementById("tip-per-person").textContent = calcTip();
+  document.getElementById("total-per-person").textContent = calcBill();
+}
+
+function calcTip() {
+  return Math.round((tip / people) * 100) / 100;
+}
+
+function calcBill() {
+  return Math.round(((billStartingVal + tip) / people) * 100) / 100;
+}
