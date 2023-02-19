d7d6861fd507cc15a9e47b8fdc6c65821caa0cda
danielthane <dthane8294@hotmail.co.uk>
2023-02-19 21:22:30 +0000

    Final functionality implementation.

--- index.js
+++ index.js
@@ -2,34 +2,15 @@
 //Selecting Variables
 let billCostEl = document.querySelector("#bill");
 let peopleEl = document.querySelector("#number-people");
+let customTipEl = document.querySelector("#custom-tip");
+let resetButtonEl = document.querySelector(".reset");
 let billStartingVal = billCostEl.value;
 let people = peopleEl.value;
 let tipPercentage = 0;
 let tip;
 let finalBill;
 
-// Dynamically Change the Bill Value without tip
-billCostEl.addEventListener("input", () => {
-  billStartingVal = parseFloat(billCostEl.value);
-});
-
-//Dynamically Change the people value
-peopleEl.addEventListener("input", () => {
-  people = parseFloat(peopleEl.value);
-  if (people > 0) {
-    tip = billStartingVal * (tipPercentage / 100);
-    updateResults();
-  }
-});
-
-//Add event listeners to each tip button
-document.querySelectorAll(".tip").forEach((item) => {
-  item.addEventListener("click", () => {
-    tipPercentage = item.innerHTML.split("%")[0];
-    updateResults();
-  });
-});
-
+//Function Definitions
 function updateResults() {
   if (people > 0) {
     tip = billStartingVal * (tipPercentage / 100);
@@ -45,3 +26,45 @@ function calcTip() {
 function calcBill() {
   return Math.round(((billStartingVal + tip) / people) * 100) / 100;
 }
+
+// DOM MANIPULATION
+// Dynamically Change the Bill Value without tip
+billCostEl.addEventListener("input", () => {
+  billStartingVal = parseFloat(billCostEl.value);
+});
+
+//Dynamically Change the people value
+peopleEl.addEventListener("input", () => {
+  people = parseFloat(peopleEl.value);
+  if (people > 0) {
+    tip = billStartingVal * (tipPercentage / 100);
+    updateResults();
+  }
+});
+
+//Custom Tip stuff
+customTipEl.addEventListener("input", () => {
+  tipPercentage = customTipEl.value;
+  updateResults();
+});
+
+//Add event listeners to each tip button
+document.querySelectorAll(".tip").forEach((item) => {
+  item.addEventListener("click", () => {
+    tipPercentage = item.innerHTML.split("%")[0];
+    updateResults();
+  });
+});
+
+//Reset Button
+resetButtonEl.addEventListener("click", () => {
+  billCostEl.value = 0;
+  peopleEl.value = 0;
+  customTipEl.value = 0;
+  tipPercentage = 0;
+  billStartingVal = 0;
+  people = 0;
+  finalBill = 0;
+  tip = 0;
+  updateResults();
+});
